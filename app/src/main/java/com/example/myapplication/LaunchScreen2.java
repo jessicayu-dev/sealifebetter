60ad083f304db0dab8b56cec630fe4721900889b
Sidharth-S-Kumar <77217570+Sidharth-S-Kumar@users.noreply.github.com>
2021-01-16 16:03:55 -0800

    LaunchScreen2 Initial Layout and Strings are set up

---
+++ app/src/main/java/com/example/myapplication/LaunchScreen2.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class LaunchScreen2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_screen2);
+    }
+}
--- app/src/main/java/com/example/myapplication/MoodAdapter.java
+++ app/src/main/java/com/example/myapplication/MoodAdapter.java
@@ -1,13 +1,30 @@
 package com.example.myapplication;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MoodAdapter extends RecyclerView.Adapter<MoodAdapter.MoodHolder> {
 
 
+    @NonNull
+    @Override
+    public MoodHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MoodHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     class MoodHolder extends RecyclerView.ViewHolder {
         private TextView id;
